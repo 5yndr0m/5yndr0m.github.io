04f6da841ee74ca17fba5353c87d44f3e49af185
5yndr0m <sdilanjana21@gmail.com>
2026-02-04 15:51:00 +0530

    feat: add first blog post and fix render issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,16 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    category: z.string().default("General"),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      // Use the image helper here
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(), // Now it expects a local file path
+      category: z.string().default("General"),
+    }),
 });
 
 export const collections = { blog };
